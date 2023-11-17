8c810c058076dbca6665aef5b28176755bdf2d7a
valentin di domenica <valendido69@gmail.com>
2023-11-17 20:22:34 -0300

    frist try deplot

--- src/components/Card/Card.test.js
+++ src/components/Card/Card.test.js
@@ -1,7 +1,7 @@
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Card from './CArd';
+import Card from './Card';
 
 describe('Card', () => {
   const mockDeletePoke = jest.fn();
